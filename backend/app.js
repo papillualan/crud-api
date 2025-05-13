942eb11c22fb71e45a6cad19c22655e835416e4f
ext35610315 <ext35610315>
2025-05-13 16:38:26 -0300

    Actualizo app.js y elimina .env.example

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config();  // Carga las variables de entorno desde el archivo .env
+dotenv.config();  // Cargo las variables de entorno desde el archivo .env
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Usa la cadena de conexión de Atlas desde el archivo .env
+// Conexión de Atlas desde el archivo .env
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB conectado con éxito'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
